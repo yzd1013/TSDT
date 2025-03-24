ddb701b0c0fdb60a3d5f80aba5b6548165e58c18
zande_zd <3094464887@qq.com>
2025-03-24 15:12:25 +0800

    message 2:The first functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,28 @@
 from selenium import webdriver
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+import unittest
 
-assert 'Django' in browser.page_source
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 张三听说有一个在线待办事项的应用
+        # 他去看了这个应用的首页
+        self.browser.get('http://localhost:8000')
+
+        # 他注意到网页里包含"To-Do"这个词
+        self.assertIn('To-Do', self.browser.title),"browser title was " + self.browser.title
+        self.fail('Finish the test!')
+
+        #应用有一个输入待办事项的文本输入框
+
+        #他在文本框中输入了"Buy flowers"
+        
+        #他想访问那个URL，发现他的待办事项列表还在
+        #他满意的离开了
+
+if __name__ == '__main__':
+    unittest.main()
