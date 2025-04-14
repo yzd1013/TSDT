1f23c37531caad9970ce4e7682528bc85c2faeaa
zande_zd <3094464887@qq.com>
2025-04-14 10:56:11 +0800

    message 14: add a new user

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -70,6 +70,49 @@ class NewVisitorTest(LiveServerTestCase):
         # 他访问那个URL，发现他的待办事项列表还在
         # 他满意的离开了
 
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        #张三新建一个待办事项清单
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Buy flowers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy flowers')
+
+        # 他注意到他的清单有一个唯一的URL
+        zhangsan_list_url = self.browser.current_url
+        self.assertRegex(zhangsan_list_url, '/lists/.+')
+
+        #现在一个新用户王五访问网站
+        #我们使用一个新的浏览器会话
+        #确保张三的信息不回从cookie中泄露出去
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+
+        #王五访问首页
+        #页面中看不到张三的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy flowers', page_text)
+        self.assertNotIn('Give a gift to Lisi', page_text)
+
+        #王五输入一个新待办事项，新建一个清单
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        #王五获得了一个唯一的URL
+        wangwu_list_url = self.browser.current_url
+        self.assertRegex(wangwu_list_url, '/lists/.+')
+        self.assertNotEqual(wangwu_list_url, zhangsan_list_url)
+
+        #页面中还是没有张三的清单
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy flowers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        #两人都很满意，然后去睡觉了
+        
 
 # if __name__ == '__main__':
 #     unittest.main()
