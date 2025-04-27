0a49edd55a772781bc79268660adbcc446e149ae
zande_zd <3094464887@qq.com>
2025-04-27 20:02:37 +0800

    message 16: form URL of new list by POST

--- notes/urls.py
+++ notes/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from lists import views
 urlpatterns = [
     path('',views.home_page,name='home'),
+    path('lists/new',views.new_list,name='new_list'),
     path('lists/the-new-page/',views.view_list,name='view_list')
 ]
