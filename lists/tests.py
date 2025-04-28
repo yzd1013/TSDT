e25df80b3fd2281811996ffdd8c70e7bf7c84dec
zande_zd <3094464887@qq.com>
2025-04-28 09:28:30 +0800

    message 17:change models for multiple users

--- lists/tests.py
+++ lists/tests.py
@@ -3,17 +3,25 @@ from django.urls import resolve
 from lists.views import home_page 
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item,List
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_user = List()
+        list_user.save()
+        
         first_item = Item()
         first_item.text = 'The first list item'
+        first_item.list = list_user
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_user
         second_item.save()
+        
+        saved_list=List.objects.first()
+        self.assertEqual(saved_list, list_user)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -21,7 +29,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first list item')
+        self.assertEqual(first_saved_item.list, list_user)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_user)
 
 class HomePageTest(TestCase):
     # def test_dispalys_all_list_items(self):
@@ -58,17 +68,19 @@ class HomePageTest(TestCase):
     #     self.assertEqual(Item.objects.count(),0)
         
 class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response=self.client.get('/lists/the-new-page/')
+        self.assertTemplateUsed(response, 'list.html')
     
     def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_user=List.objects.create()
+        Item.objects.create(text='itemey 1',list=list_user)
+        Item.objects.create(text='itemey 2',list=list_user)
         response = self.client.get('/lists/the-new-page/')
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
         
-    def test_uses_list_template(self):
-        response=self.client.get('/lists/the-new-page/')
-        self.assertTemplateUsed(response, 'list.html')
+    
         
 class NewListTest(TestCase):
     
