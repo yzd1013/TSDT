16bc4c1d169b7e61e002d100e784750e938365b1
zande_zd <3094464887@qq.com>
2025-05-13 19:03:36 +0800

    message 23: FT cantest real_server

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -6,13 +6,16 @@ from selenium.webdriver.common.by import By
 # from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import WebDriverException
+import os
 
 MAX_WAIT=10
 
 class NewVisitorTest(StaticLiveServerTestCase): # 继承 StaticLiveServerTestCase
     def setUp(self):
         self.browser = webdriver.Chrome()
-
+        real_server = os.environ.get('REAL_SERVER')
+        if real_server:
+            self.live_server_url = 'http://' + real_server
     def tearDown(self):
         self.browser.quit()
 
