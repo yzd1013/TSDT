737165a4d82685ea84a1a215e1e61ae973ec2d82
zande_zd <3094464887@qq.com>
2025-05-20 20:56:29 +0800

    message 26: add gunicorn to virtualenv requirements

--- notes/settings.py
+++ notes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-32f6hmx=0gno776@(#$7+%rhd-+s6@6h$9@h^z=oalv-$^_(3q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['182.92.79.45']
 
 
 # Application definition
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'../static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
